88923ccabfb88282bb47a363334f23d6d16f9874
superanika <anamaria.romerogarcia@gmail.com>
2019-04-08 09:33:02 +0200

    create adivina function

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 'use strict';
 
 const counter = document.querySelector('.counter').innerHTML;
-const input = document.querySelector('#guess').value;
+const input = document.querySelector('#guess');
 const button = document.querySelector('.btn');
-const message = document.querySelector('.message').innerHTML;
+const message = document.querySelector('.message');
+const randomNumber = getRandomNumber(100);
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+
+  console.log('> '+ randomNumber);
+  
 
 function adivina () {
 
+    const inputNumber = parseInt(input.value);
+
     
+    if (inputNumber < randomNumber) {
+        message.innerHTML = 'Demasiado bajo';
+    }
+    else if (inputNumber > randomNumber) {
+        message.innerHTML = 'Demasiado alto';
+    }
+    else  {
+        message.innerHTML = '¡has ganado campeona!';
+    }
+
 }
 
 button.addEventListener('click', adivina);
