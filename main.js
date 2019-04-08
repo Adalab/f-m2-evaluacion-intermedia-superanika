4e56193eaa9876ab2b27853104c7fd2e849e666f
superanika <anamaria.romerogarcia@gmail.com>
2019-04-08 08:37:15 +0200

    create html and js variables

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const counter = document.querySelector('.counter').innerHTML;
+const input = document.querySelector('#guess').value;
+const button = document.querySelector('.btn');
+const message = document.querySelector('.message').innerHTML;
+
+function adivina () {
+
+    
+}
+
+button.addEventListener('click', adivina);
